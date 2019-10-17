64bcca70d72799526918a2c214e6d676375e2f43
Ariz Chang <abchang@asu.edu>
2019-10-17 15:43:45 -0700

    filled in array of storm event structs

--- defn.h
+++ defn.h
@@ -4,7 +4,8 @@
 #define	COUNTY_LEN	30
 #define	TOR_LEN		4
 #define DATE_LEN    20
-#define FAC_LOC     25
+#define FAT_LOC     25
+using namespace std;
 
 struct annual_storms{
 	int year; // Year of storm events
@@ -13,19 +14,19 @@ struct annual_storms{
 
 struct storm_event{
 	int event_id; // Event id
-	char state[ STATE_LEN ]; // State name
-	int year; // Four digit year of event
-	char month_name[ MONTH_LEN ]; // Month of event
-	char event_type[ EVENT_LEN ]; // Event type
+  string state;// State name
+  int year; // Four digit year of event
+	string  month_name; // Month of event
+	string event_type; // Event type
 	char cz_type; // Where event happened, C, Z, or M
-	char cz_name[ COUNTY_LEN ]; // Name of county/zone
+	string cz_name; // Name of county/zone
 	int injuries_direct; // Number of direct injuries
 	int injuries_indirect; // Number of indirect injuries
 	int deaths_direct; // Number of direct deaths
 	int deaths_indirect; // Number of indirect deaths
 	int damage_property; // Amount of property damage; convert to integer
 	int damage_crops; // Amount of crop damage; convert to integer
-	char tor_f_scale[ TOR_LEN ]; // Strength of tornado on Fujita scale
+	string tor_f_scale; // Strength of tornado on Fujita scale
     struct fatality_event *f; // Linked list of fatalities associated with this storm event
 };
 
@@ -36,13 +37,15 @@ struct fatality_event{
     char fatality_date[ DATE_LEN ]; // Date and time of fatality
     int fatality_age; // Age of fatality
     char fatality_sex; // Gender of fatality
-  char fatality_location[ FAC_LOC ]; //Location of fatality
+  char fatality_location[ FAT_LOC ]; //Location of fatality
+  struct fatality_event *next; //poirnter to next fatality event
 };
 
 struct hash_table_entry{
     int event_id; // Event id -- key used to hash on
     int year; // Year of storm event
     int event_index; // For the given year, the index into array of storm events
+  struct hash_table_entry *next; //pointer to next entry in case of collisions
 };
 
 struct bst{ // A binary search tree
--- storm.cpp
+++ storm.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <sstream>
 #include <vector>
 #include "defn.h"
+
+int numberfy(string);
 
 using namespace std;
 
@@ -29,21 +32,6 @@ int main(int argc, char** argv)
   //file reading
   ifstream input(file);
 
-  int eventId;
-  string state;
-  int year;
-  string month_name;
-  string event_type;
-  char cz_type;
-  string cz_name;
-  int injuries_direct;
-  int injuries_indirect;
-  int deaths_direct;
-  int deaths_indirect;
-  int damage_property;
-  int damage_crops;
-  string tor_f_scale;
-
   if(!input.is_open())
     exit(EXIT_FAILURE);
 
@@ -56,11 +44,79 @@ int main(int argc, char** argv)
       string token;
       while(getline(iss, token, ','))
 	{
-	  cout << token << " ";
+	  //cout << token << " ";
 	}
       lineCount++;
-      cout << endl;
+      //cout << endl;
     }
-  cout << "Line count: " << lineCount << endl;
+  //cout << "Line count: " << lineCount << endl;
+
+  ifstream data(file);
+
+  storm_event** stormEvents = new storm_event*[lineCount];
+  int i = 0;
+  getline(data, str); //skip first line
+  while(getline(data, str))
+    {
+      istringstream iss(str);
+      string token;
+      storm_event* event = new storm_event;
+
+      getline(iss, token, ',');
+      event->event_id = stoi(token);
+      getline(iss, token, ',');
+      event->state = token;
+      getline(iss, token, ',');
+      event->year = stoi(token);
+      getline(iss, token, ',');
+      event->month_name = token;
+      getline(iss, token, ',');
+      event->event_type = token;
+      getline(iss, token, ',');
+      event->cz_type = token[0];
+      getline(iss, token, ',');
+      event->cz_name = token;
+      getline(iss, token, ',');
+      event->injuries_direct = stoi(token);
+      getline(iss, token, ',');
+      event->injuries_indirect = stoi(token);
+      getline(iss, token, ',');
+      event->deaths_direct = stoi(token);
+      getline(iss, token, ',');
+      event->deaths_indirect =  stoi(token);
+      getline(iss, token, ',');
+      event->damage_property = numberfy(token);
+      getline(iss, token, ',');
+      event->damage_crops = numberfy(token);
+      getline(iss, token, ',');
+      event->tor_f_scale = token;
+
+      stormEvents[i] = event;
+      i++;
+    }
+
+  for(int i = 0; i < lineCount; i++)
+    {
+      cout << stormEvents[i]->event_id << " " << stormEvents[i]->state << " " << stormEvents[i]->damage_property << endl;
+    }
+
   return 0;
+}
+
+int numberfy(string str)
+{
+  float num = 0;
+  if(str[str.length()-1] == 'K')
+    {
+      str.pop_back();
+      num = stof(str);
+      num = num*1000;
+    }
+  else if(str[str.length()-1] == 'M')
+    {
+      str.pop_back();
+      num = stof(str);
+      num = num*1000000;
+    }
+  return (int)num;
 }
